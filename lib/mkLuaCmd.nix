afbdcb0454615af93de85abf3cc12031676d21ef
Ante Petrovic <petrovicante6@gmail.com>
2025-11-08 14:42:54 +0100

    test

--- lib/mkLuaCmd.nix
+++ lib/mkLuaCmd.nix
@@ -1,14 +1,20 @@
-{ lib ? import <nixpkgs/lib> {} }:
-{
-  mkLuaCmd     = module: fn: "<CMD>lua(require '${module}').${fn}()<CR>";
-  mkLuaCmdArgs = module: fn: args:
-    let
-      mkArgs =
-        if args == null || args == [] then ""
-        else "(" + lib.concatStringsSep ", " (map (x:
-          if builtins.isString x then "'${x}'" else builtins.toString x
-        ) args) + ")";
-    in "<CMD>lua(require '${module}').${fn}" + mkArgs + "<CR>";
-  mkLuaFnRaw   = module: fn: "function() require('${module}').${fn}() end";
+{lib ? import <nixpkgs/lib> {}}: {
+  mkLuaCmd = module: fn: "<CMD>lua(require '${module}').${fn}()<CR>";
+  mkLuaCmdArgs = module: fn: args: let
+    mkArgs =
+      if args == null || args == []
+      then ""
+      else
+        "("
+        + lib.concatStringsSep ", " (map (
+            x:
+              if builtins.isString x
+              then "'${x}'"
+              else builtins.toString x
+          )
+          args)
+        + ")";
+  in
+    "<CMD>lua(require '${module}').${fn}" + mkArgs + "<CR>";
+  mkLuaFnRaw = module: fn: "function() require('${module}').${fn}() end";
 }
-
